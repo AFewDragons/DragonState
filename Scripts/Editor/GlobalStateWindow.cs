e9ef6a4df9e6888e58118724d948f12ff6b71d49
Keirron Stach <stachkeirron@gmail.com>
2021-03-15 10:36:44 +1030

    Add debug states
    Added ability to add new states through debug menu. Default types only to begin with.

--- Scripts/Editor/GlobalStateWindow.cs
+++ Scripts/Editor/GlobalStateWindow.cs
@@ -2,17 +2,28 @@
 using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
+using System.Reflection;
+using System.Linq;
+using System;
 
 namespace AFewDragons
 {
     public class GlobalStateWindow : EditorWindow
     {
+        private System.Type[] types;
+
         [MenuItem("Window/Global State")]
         static void Init()
         {
             // Get existing open window or if none, make a new one:
             GlobalStateWindow window = (GlobalStateWindow)GetWindow(typeof(GlobalStateWindow));
             window.Show();
+        }
+
+        private void SetTypes()
+        {
+            var allTypes = Assembly.GetAssembly(typeof(GlobalStateBase)).GetTypes();
+            types = (from System.Type type in allTypes where type.IsSubclassOf(typeof(GlobalStateBase)) select type).Where(t => !t.Name.Contains("GlobalStateBase")).ToArray();
         }
 
         private void Awake()
@@ -34,7 +45,8 @@ namespace AFewDragons
         {
             if (EditorApplication.isPlaying)
             {
-                if(GlobalStateManager.GetState() != null && GlobalStateManager.GetState().State != null)
+                if (types == null) SetTypes();
+                if (GlobalStateManager.GetState() != null && GlobalStateManager.GetState().State != null)
                 {
                     string removeState = null;
                     foreach (var state in GlobalStateManager.GetState().State)
@@ -46,6 +58,55 @@ namespace AFewDragons
                         }
                         EditorGUILayout.EndHorizontal();
                     }
+                    //New state
+                    EditorGUILayout.BeginHorizontal();
+                    EditorGUILayout.LabelField("");
+                    EditorGUILayout.EndHorizontal();
+
+                    EditorGUILayout.BeginHorizontal();
+                    EditorGUILayout.LabelField("Add new type", GUILayout.Width(110));
+                    var dropdownRect = EditorGUILayout.GetControlRect();
+                    if (EditorGUI.DropdownButton(dropdownRect, new GUIContent(newStateType ?? "Type"), FocusType.Passive)){
+                        var menu = new GenericMenu();
+
+                        foreach (var type in types)
+                        {
+
+                            menu.AddItem(new GUIContent(type.Name), type.Name == newStateType, OnStateTypeSelected, type.Name);
+                        }
+                        menu.DropDown(dropdownRect);
+                    }
+                    if (!string.IsNullOrEmpty(newStateType))
+                    {
+                        newStateKey = EditorGUILayout.TextField(newStateKey);
+                        switch (newStateType)
+                        {
+                            case "GlobalStateFloat":
+                                newStateValue = EditorGUILayout.FloatField(newStateValue != null ? (float)newStateValue : 0);
+                                break;
+                            case "GlobalStateString":
+                                newStateValue = EditorGUILayout.TextField(newStateValue != null ? (string)newStateValue : "");
+                                break;
+                            case "GlobalStateInt":
+                                newStateValue = EditorGUILayout.IntField(newStateValue != null ? (int)newStateValue : 0);
+                                break;
+                            case "GlobalStateBool":
+                                newStateValue = GUILayout.Toggle(newStateValue != null ? (bool)newStateValue : false, GUIContent.none);
+                                break;
+                            default:
+                                EditorGUILayout.LabelField("Not implemented.", GUILayout.Width(110));
+                                break;
+                        }
+                        if (GUILayout.Button("Add", GUILayout.Width(45)))
+                        {
+                            GlobalStateManager.Set(newStateKey, newStateValue);
+                            newStateType = null;
+                            newStateValue = null;
+                            newStateKey = "";
+                        }
+                    }
+                    EditorGUILayout.EndHorizontal();
+                    
                     if (!string.IsNullOrEmpty(removeState))
                     {
                         GlobalStateManager.Remove(removeState);
@@ -57,7 +118,15 @@ namespace AFewDragons
             {
                 EditorGUILayout.LabelField("Game not running.");
             }
-            
+        }
+
+        private string newStateKey = "";
+        private object newStateValue;
+        private string newStateType;
+
+        private void OnStateTypeSelected(object stateType) {
+            newStateType = (string)stateType;
+            newStateValue = null;
         }
     }
 }
